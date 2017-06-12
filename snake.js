522a0a044f214fcf17cb733c0df26fb7fc9f32e7
roelin <506627717@QQ.com>
2017-06-12 18:42:47 +0800

    fwhup
    添加贪吃蛇移动方法、绘制方法
    根据判断条件确定是吃食物还是死亡

--- snake.js
+++ snake.js
@@ -84,5 +84,106 @@
                 }  
             }  
    };  
+   
+   //move function  
+	move = function(){  
+		Snake.timer = setInterval(function(){        
+			erase();  
+            moveOneStep();              
+			paint();  
+			}, Snake.speed);  
+    };  
+	
+	//move body one step 
+	moveOneStep =  function(){  
+		if(checkNextStep()==-1){  
+			clearInterval(Snake.timer);              
+			alert("Game over!\n");
+			window.location.reload(); 
+			return;           
+		} 
+		if(checkNextStep()==1){                
+			var _point = getNextPos();              
+			var _x = _point.x;               
+			var _y = _point.y;  			
+			var _color = getColor(_x,_y);    
+			Snake.body.unshift({x:_x,y:_y,color:_color});  
+			//eat food , create food 
+			var foodx = Math.floor(Math.random()*Snake.colCount);  
+			var foody = Math.floor(Math.random()*Snake.rowCount);  			
+			Snake.tbl.rows[foody].cells[foodx].style.backgroundColor = "red"; 
+			return;  
+        }  
+           
+            var point = getNextPos();  
+             var color = Snake.body[0].color;   
+            for(var i=0; i<Snake.body.length-1; i++){  
+                    Snake.body[i].color = Snake.body[i+1].color;  
+            }  
+            //add snake end one dot  
+            Snake.body.pop();  
+            Snake.body.unshift({x:point.x,y:point.y,color:color}); 
+			Snake.body[0].color = "black";
+			if(Snake.body.length>1)
+				Snake.body[Snake.body.length-1].color = "black";
+            //window.status = toString();  
+        };  
+		
+		
+    //Next position function         
+	getNextPos =  function(){  
+		var x = Snake.body[0].x;  
+		var y = Snake.body[0].y;  	
+		var color = Snake.body[0].color;  
+		//to up  
+		if(Snake.direction==0){  
+			y--;  
+		}  
+		//to right  
+		else if(Snake.direction==1){  
+			x++;  
+		}  
+		//to down  
+		else if(Snake.direction==2){  
+			y++;  
+		}  
+		//to left  
+		else{  
+			x--;  
+		}  
+		//return a location  
+		return {x:x,y:y};  
+	};  
+        
+           
+		
+  //erase snake body
+	erase = function(){  
+		for(var i=0; i<Snake.body.length; i++){  
+			eraseDot(Snake.body[i].x, Snake.body[i].y);  
+        }  
+    }; 
+    
+    //paint snake body
+	paint = function(){  
+		for(var i=0; i<Snake.body.length; i++){     
+			paintDot(Snake.body[i].x, Snake.body[i].y,Snake.body[i].color);  
+        }      
+	}; 
+       
+   //erase one dot
+   eraseDot = function(x,y){  
+		Snake.tbl.rows[y].cells[x].style.backgroundColor = "";  
+	}; 
+	//paint one dot
+    paintDot = function(x,y,color){  
+		Snake.tbl.rows[y].cells[x].style.backgroundColor = color;      
+	};  
+	//get location color
+	getColor = function(x,y){  
+		return Snake.tbl.rows[y].cells[x].style.backgroundColor;  
+	};  
+  
+		
 	
 	
