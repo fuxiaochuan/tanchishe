06b3fc01eb8c54725786cba74a2c442cd6f222b1
fuxiaochuan <527629323@qq.com>
2017-06-12 18:47:38 +0800

    Create snake.js

--- snake.js
+++ snake.js
@@ -1,44 +1,44 @@
     function $(id){return document.getElementById(id);} 
-    //Ì°³ÔÉßÀà  
+    //è´ªåƒè›‡ç±»  
     var Snake = {  
         tbl: null,  
         body: [],  
-        //µ±Ç°ÒÆ¶¯µÄ·½Ïò,È¡Öµ0,1,2,3, ·Ö±ğ±íÊ¾ÏòÉÏ,ÓÒ,ÏÂ,×ó, °´¼üÅÌ·½Ïò¼ü¿ÉÒÔ¸Ä±äËü  
+        //å½“å‰ç§»åŠ¨çš„æ–¹å‘,å–å€¼0,1,2,3, åˆ†åˆ«è¡¨ç¤ºå‘ä¸Š,å³,ä¸‹,å·¦, æŒ‰é”®ç›˜æ–¹å‘é”®å¯ä»¥æ”¹å˜å®ƒ  
         direction: 0,  
-        //¶¨Ê±Æ÷  
+        //å®šæ—¶å™¨  
         timer: null,  
-        //ËÙ¶È  
+        //é€Ÿåº¦  
         speed: 250,  
-        //ĞĞÊı  
+        //è¡Œæ•°  
         rowCount: 30,  
-        //ÁĞÊı  
+        //åˆ—æ•°  
         colCount: 30,  
         
     }; 
 	
-	//³õÊ¼»¯  
+	//åˆå§‹åŒ–  
 	init = function(){  
   
         Snake.tbl = $("main");  
         var x = 0;  
         var y = 0;  
         var colorIndex = 0; 
-        //²úÉú³õÊ¼ÒÆ¶¯·½Ïò  
+        //äº§ç”Ÿåˆå§‹ç§»åŠ¨æ–¹å‘  
         Snake.direction = Math.floor(Math.random()*4);  
-        //¹¹Ôìtable  
+        //æ„é€ table  
         for(var row=0;row<Snake.rowCount;row++){  
             var tr=Snake.tbl.insertRow(-1);  
             for(var col=0;col<Snake.colCount;col++) {  
                 var td=tr.insertCell(-1);  
             }  
         }  
-        //²úÉú½Úµã  
+        //äº§ç”ŸèŠ‚ç‚¹  
          x = Math.floor(Math.random()*Snake.colCount);  
          y = Math.floor(Math.random()*Snake.rowCount);   
       
          Snake.tbl.rows[y].cells[x].style.backgroundColor = 'red';  
               
-        //²úÉúÉßÍ·  
+        //äº§ç”Ÿè›‡å¤´  
         while(true){  
             x = Math.floor(Math.random()*Snake.colCount);  
             y = Math.floor(Math.random()*Snake.rowCount);  
@@ -47,12 +47,12 @@
             break;  
           
         }  
-        //Ìí¼Ó¼üÅÌÊÂ¼ş  
+        //æ·»åŠ é”®ç›˜äº‹ä»¶  
 		document.onkeydown= function(e){  
 			if (!e)e=window.event;             
 			switch(e.keyCode | e.which | e.charCode){  
                     case 37:{//left  
-                        //×èÖ¹Éßµ¹ÍË×ß  
+                        //é˜»æ­¢è›‡å€’é€€èµ°  
                         if(Snake.direction==1){  
                             break;  
                         }  
@@ -61,21 +61,21 @@
 	
                     }  
                     case 38:{
-                        if(Snake.direction==2){//×èÖ¹Éßµ¹ÍË×ß  
+                        if(Snake.direction==2){//é˜»æ­¢è›‡å€’é€€èµ°  
                             break;  
                         }  
                         Snake.direction = 0;  
                         break;  
                     }  
                     case 39:{//right  
-                        if(Snake.direction==3){//×èÖ¹Éßµ¹ÍË×ß  
+                        if(Snake.direction==3){//é˜»æ­¢è›‡å€’é€€èµ°  
                             break;  
                         }  
                         Snake.direction = 1;  
                         break;  
                     }  
                     case 40:{//down  
-                        if(Snake.direction==0){//×èÖ¹Éßµ¹ÍË×ß  
+                        if(Snake.direction==0){//é˜»æ­¢è›‡å€’é€€èµ°  
                             break;  
                         }  
                         Snake.direction = 2;  
@@ -85,105 +85,6 @@
             }  
    };  
    
-   //move function  
-	move = function(){  
-		Snake.timer = setInterval(function(){        
-			erase();  
-            moveOneStep();              
-			paint();  
-			}, Snake.speed);  
-    };  
-	
-	//move body one step 
-	moveOneStep =  function(){  
-		if(checkNextStep()==-1){  
-			clearInterval(Snake.timer);              
-			alert("Game over!\n");
-			window.location.reload(); 
-			return;           
-		} 
-		if(checkNextStep()==1){                
-			var _point = getNextPos();              
-			var _x = _point.x;               
-			var _y = _point.y;  			
-			var _color = getColor(_x,_y);    
-			Snake.body.unshift({x:_x,y:_y,color:_color});  
-			//eat food , create food 
-			var foodx = Math.floor(Math.random()*Snake.colCount);  
-			var foody = Math.floor(Math.random()*Snake.rowCount);  			
-			Snake.tbl.rows[foody].cells[foodx].style.backgroundColor = "red"; 
-			return;  
-        }  
-           
-            var point = getNextPos();  
-             var color = Snake.body[0].color;   
-            for(var i=0; i<Snake.body.length-1; i++){  
-                    Snake.body[i].color = Snake.body[i+1].color;  
-            }  
-            //add snake end one dot  
-            Snake.body.pop();  
-            Snake.body.unshift({x:point.x,y:point.y,color:color}); 
-			Snake.body[0].color = "black";
-			if(Snake.body.length>1)
-				Snake.body[Snake.body.length-1].color = "black";
-            //window.status = toString();  
-        };  
-		
-		
-    //Next position function         
-	getNextPos =  function(){  
-		var x = Snake.body[0].x;  
-		var y = Snake.body[0].y;  	
-		var color = Snake.body[0].color;  
-		//to up  
-		if(Snake.direction==0){  
-			y--;  
-		}  
-		//to right  
-		else if(Snake.direction==1){  
-			x++;  
-		}  
-		//to down  
-		else if(Snake.direction==2){  
-			y++;  
-		}  
-		//to left  
-		else{  
-			x--;  
-		}  
-		//return a location  
-		return {x:x,y:y};  
-	};  
-        
-           
-		
-  //erase snake body
-	erase = function(){  
-		for(var i=0; i<Snake.body.length; i++){  
-			eraseDot(Snake.body[i].x, Snake.body[i].y);  
-        }  
-    }; 
-    
-    //paint snake body
-	paint = function(){  
-		for(var i=0; i<Snake.body.length; i++){     
-			paintDot(Snake.body[i].x, Snake.body[i].y,Snake.body[i].color);  
-        }      
-	}; 
-       
-   //erase one dot
-   eraseDot = function(x,y){  
-		Snake.tbl.rows[y].cells[x].style.backgroundColor = "";  
-	}; 
-	//paint one dot
-    paintDot = function(x,y,color){  
-		Snake.tbl.rows[y].cells[x].style.backgroundColor = color;      
-	};  
-	//get location color
-	getColor = function(x,y){  
-		return Snake.tbl.rows[y].cells[x].style.backgroundColor;  
-	};  
-  
 		
 	
 	
