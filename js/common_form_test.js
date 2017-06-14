ec6074337849f9b619554c8ca275214b66e033df
roelin <506627717@QQ.com>
2017-06-14 16:15:22 +0800

    up
    up

--- js/common_form_test.js
+++ js/common_form_test.js
@@ -1,21 +1,27 @@
-// 全局变量a和b，分别获取用户框和密码框的value值
-var a = document.getElementsByTagName("input")[0].value;
-var b = document.getElementsByTagName("input")[1].value;
-
+// 全局变量a和b，分别获取用户框和密码框的value值l;
+var User = {
+	usr : null,
+	pwd : null,
+};
 //用户框失去焦点后验证value值
 function oBlur_1() {
-    if (!a) { //用户框value值为空
+	User.usr = document.getElementsByTagName("input")[0].value;
+	mUser = User.usr;
+    if (!User.usr) { //用户框value值为空
         document.getElementById("remind_1").innerHTML = "请输入用户名！";
         document.getElementById("change_margin_1").style.marginBottom = 1 + "px";
     } else { //用户框value值不为空
         document.getElementById("remind_1").innerHTML = "";
         document.getElementById("change_margin_1").style.marginBottom = 19 + "px";
+		document.login.action = "index.html?user="+User.usr;
     }
 }
 
 //密码框失去焦点后验证value值
 function oBlur_2() {
-    if (!b) { //密码框value值为空
+	//b = document.getElementsByTagName("input")[1].value;
+	User.pwd = document.getElementsByTagName("input")[1].value;;
+    if (!User.pwd) { //密码框value值为空
         document.getElementById("remind_2").innerHTML = "请输入密码！";
         document.getElementById("change_margin_2").style.marginBottom = 1 + "px";
         document.getElementById("change_margin_3").style.marginTop = 2 + "px";
@@ -40,22 +46,30 @@ function oFocus_2() {
 }
 
 //若输入框为空，阻止表单的提交
-function submitTest() {
-    if (!a && !b) { //用户框value值和密码框value值都为空
+function submitLog() {
+    if (!User.usr && !User.pwd) { //用户框value值和密码框value值都为空
         document.getElementById("remind_1").innerHTML = "请输入用户名！";
         document.getElementById("change_margin_1").style.marginBottom = 1 + "px";
         document.getElementById("remind_2").innerHTML = "请输入密码！";
         document.getElementById("change_margin_2").style.marginBottom = 1 + "px";
         document.getElementById("change_margin_3").style.marginTop = 2 + "px";
         return false; //只有返回true表单才会提交
-    } else if (!a) { //用户框value值为空
+    } else if (!User.usr) { //用户框value值为空
         document.getElementById("remind_1").innerHTML = "请输入用户名！";
         document.getElementById("change_margin_1").style.marginBottom = 1 + "px";
         return false;
-    } else if (!b) { //密码框value值为空
+    } else if (!User.pwd) { //密码框value值为空
         document.getElementById("remind_2").innerHTML = "请输入密码！";
         document.getElementById("change_margin_2").style.marginBottom = 1 + "px";
         document.getElementById("change_margin_3").style.marginTop = 2 + "px";
         return false;
     }
+	else{
+		alert("登录成功,准备进入游戏");
+		document.login.action = "index.html?user="+User.usr;
+		return true;
+	}
 }
+
+  
+
